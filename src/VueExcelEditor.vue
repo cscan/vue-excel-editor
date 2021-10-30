06d7ea5dfed9977b7c10d8ce8fb345495811ca94
Kenneth Cheng <kenneth.cheng@ortholite.hk>
2021-10-30 14:59:51 +0800

    fix lengthLimit

--- src/VueExcelEditor.vue
+++ src/VueExcelEditor.vue
@@ -1439,7 +1439,7 @@ export default {
               else
                 if (this.currentField.allowKeys.indexOf(e.key.toUpperCase()) === -1) return e.preventDefault()
             }
-            if (this.currentField.lengthLimit && this.inputBox.value.length > this.currentField.lengthLimit) return e.preventDefault()
+            if (this.currentField.lengthLimit && this.inputBox.value.length >= this.currentField.lengthLimit) return e.preventDefault()
             if (!this.inputBoxShow) {
               if (this.currentField.type === 'select' || this.currentField.type === 'map') {
                 setTimeout(() => this.calAutocompleteList(true))
